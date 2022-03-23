fde90b72c9c7035421bb36fa54e91c67754d6232
Stefan Alexandru <stefanalexandru02@outlook.com>
2022-03-23 19:09:55 +0200

    exercitiu laborator

---
+++ week5/Lab/Complex.cpp
@@ -0,0 +1,144 @@
+#include "Complex.h"
+
+bool double_equals(double l, double r)
+{
+    return abs(l - r) < 0.001;
+}
+
+Complex operator+(const Complex& l, const Complex& r)
+{
+    return Complex(l.real() + r.real(), l.imag() + r.imag());
+}
+
+Complex operator+(const Complex& l, double r)
+{
+    return l + Complex(r, 0);
+}
+
+Complex operator+(double l, const Complex& r)
+{
+    return Complex(l,0) + r;
+}
+
+Complex operator-(const Complex& l, const Complex& r)
+{
+    return Complex(l.real() - r.real(), l.imag() - r.imag());
+}
+
+Complex operator-(const Complex& l, double r)
+{
+    return l - Complex(r, 0);
+}
+
+Complex operator-(double l, const Complex& r)
+{
+    return Complex(l, 0) - r;
+}
+
+Complex operator*(const Complex& l, const Complex& r)
+{
+    return Complex(l.real() * r.real() - l.imag() * r.imag(), (l.real() * r.imag() + r.real()*l.imag()));
+}
+
+Complex operator*(const Complex& l, double r)
+{
+    return l * Complex(r, 0);
+}
+
+Complex operator*(double l, const Complex& r)
+{
+    return Complex(l, 0) * r;
+}
+
+Complex operator-(const Complex& obj)
+{
+    return Complex(-obj.real(), -obj.imag());
+}
+
+bool operator==(const Complex& l, const Complex& r)
+{
+    return (l.real() == r.real() && l.imag() == r.imag());
+}
+
+bool operator!=(const Complex& l, const Complex& r)
+{
+    return !(l == r);
+}
+
+std::ostream& operator<<(std::ostream& out, const Complex& complex)
+{
+    out << complex.real() << ',' << complex.imag();
+    return out;
+}
+
+Complex::Complex() : Complex(0, 0) { }
+
+Complex::Complex(double real, double imag)
+{
+    real_data = real;
+    imag_data = imag;
+}
+
+bool Complex::is_real() const
+{
+    return imag() == 0;
+}
+
+double Complex::real() const
+{
+    return real_data;
+}
+
+Complex& Complex::Set(double real, double imag)
+{
+    this->real_data = real;
+    this->imag_data = imag;
+    return *this;
+}
+
+double Complex::imag() const
+{
+    return imag_data;
+}
+
+double Complex::abs() const
+{
+    return sqrt(real() * real() + imag() * imag());
+}
+
+Complex Complex::conjugate() const
+{
+    return { real(), -imag() };
+}
+
+Complex& Complex::operator--()
+{
+    Set(real()-1, imag());
+    return *this;
+}
+
+Complex& Complex::operator--(int)
+{
+    Complex copy = *this;
+    --(*this);
+    return copy;
+}
+
+Complex& Complex::operator++()
+{
+    Set(real() + 1, imag());
+    return *this;
+}
+
+Complex& Complex::operator++(int)
+{
+    Complex copy = *this;
+    ++(*this);
+    return copy;
+}
+
+Complex& Complex::operator()(double real, double imag)
+{
+    Set(real, imag);
+    return *this;
+}
--- week5/Lab/Source.cpp
+++ week5/Lab/Source.cpp
@@ -1,99 +1,61 @@
+#include "Complex.h"
 #include <iostream>
-
-using namespace std;
-
-class MyNumber
-{
-    int x;
-
-  public:
-    int GetX() const { return x; }
-    MyNumber(int value) : x(value) {}        
-    operator float()
-    {
-        return (float) this->x;
-    }
-    MyNumber operator++()
-    {
-        x++;
-        return *this;
-    }
-    MyNumber operator ++(int)
-    {
-        MyNumber copy = *this;
-        ++(*this);
-        return copy;
-    }
-
-    MyNumber& Set(int value)
-    {
-        x = value;
-        return *this;
-    }
-    MyNumber& Add(int value)
-    {
-        this->x += value;
-        return *this;
-    }
-    void Print()
-    {
-        cout << this->x << endl;
-    }
-
-    int operator[](int index)
-    {
-        return (x >> (index - 1)) & 1;
-    }
-
-    MyNumber& operator=(int value)
-    {
-        x = value;
-        return *this;
-    }
-
-    void operator()()
-    {
-        Print();
-    }
-};
-
-MyNumber operator+(const MyNumber& stg, const MyNumber& drp)
-{
-    return { stg.GetX() + drp.GetX() };
-}
-MyNumber operator+(const int stg, const MyNumber& drp)
-{
-    return MyNumber(stg) + drp;
-}
-bool operator==(const MyNumber& stg, const MyNumber& drp)
-{
-    return stg.GetX() == drp.GetX();
-}
-bool operator!=(const MyNumber& stg, const MyNumber& drp)
-{
-    return !(stg == drp);
-}
-MyNumber& operator+=(MyNumber& stg, const int value)
-{
-    stg.Set(stg.GetX() + value); return stg;
-}
-
 int main()
 {
-    MyNumber x1(10);
-    MyNumber x2(20);
+    Complex a{ 1, 2 };
+    check(double_equals(a.real(), 1));
+    check(double_equals(a.imag(), 2));
 
-    MyNumber x3 = x1 + x2;
+    Complex b{ 2, 3 };
+    Complex c = a + b;
+    check(double_equals(c.real(), 3));
+    check(double_equals(c.imag(), 5));
 
-    int my_sum = 20 + x2;   
+    Complex d = c - a;
+    check(b == d);
 
-    float value = x1;
+    Complex e = (a * d).conjugate();
+    check(double_equals(e.imag(), -7));
 
-    x1.Set(10).Add(2).Add(2).Print();
+    // incrementeaza partea reala
+    e++;
+    ++e;
+    check(double_equals(e.real(), -2));
 
-    x1();
+    // decrementeaza partea reala
+    e--;
+    --e;
+    check(double_equals(e.real(), -4));
 
-    (x1 += 10) += 30;
+    Complex f = (a + b - d) * c;
+    if (f != Complex{ 1, 2 })
+    {
+        // printeaza in formatul a +/i bi
+        // daca a sau b sunt 0, nu vor fi printate
+        // daca ambele sunt zero, se va printa doar partea reala
+        // exemplu:                  5 + 4i
+        // sau:                     -3 - 2i
+        // daca e real:             -6
+        // daca nu are parte reala: 5i
+        std::cout << f << '\n' << a << '\n';
+        std::cout << Complex{ 1, 2 } << '\n'
+                  << Complex{ 1, -2 } << '\n'
+                  << Complex{ 0, 5 } << '\n'
+                  << Complex{ 7, 0 } << '\n'
+                  << Complex{ 0, 0 } << '\n';
+    }
+
+    // op() va updata partea reala si partea imaginara
+    // practic il folosim ca un setter pentru ambele numere
+    Complex g = f(-1, -2)(-2, -3)(-4, -5);
+    Complex h = { -4, -5 };
+    check(g == h);
+
+    Complex i = h - (h + 5) * 2;
+    check(double_equals(i.real(), -6));
+    
+    Complex j = -i + i;
+    check(double_equals(j.abs(), 0));
 
     return 0;
 }
