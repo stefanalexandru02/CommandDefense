41e9b30ca9924c063fac1c51e7f67cab03674c84
Stefan Alexandru Virna <stefanalexandru02@gmail.com>
2022-04-18 19:12:27 +0300

    add week8

---
+++ week8/homework/homework/file.in.h
@@ -0,0 +1,12 @@
+//
+//  file.in.h
+//  homework
+//
+//  Created by Stefan Alexandru on 18.04.2022.
+//
+
+#ifndef file_in_h
+#define file_in_h
+
+
+#endif /* file_in_h */
---
+++ week8/homework/homework/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework
+//
+//  Created by Stefan Alexandru on 18.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
